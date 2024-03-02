7fa8ed26efa6e948911df06ca01d8c91763cf3c9
Yasir Kaya <kayamuhammedyasir@gmail.com>
2024-03-02 16:10:20 +0300

    InMemory DB ile CRUD

--- WebApi/Book.cs
+++ WebApi/Book.cs
@@ -3,7 +3,7 @@ namespace WebApi;
 public class Book 
 {
     public int Id{ get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public int GenreId { get; set; }
     public int PageCount { get; set;}
     public DateTime PublishDate { get; set; }
--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApi.DBOperations;
 
 namespace WebApi.Controllers;
 
@@ -7,56 +8,66 @@ namespace WebApi.Controllers;
 
 public class BookController : ControllerBase
 {
-    private static List<Book> BookList = new List<Book>()
+    private readonly BookStoreDBContext _context;
+
+    public BookController(BookStoreDBContext context)
     {
-        new Book{
-            Id = 1, 
-            Title = "Lean Startup", 
-            GenreId = 1, //Personal Growth
-            PageCount = 200,
-            PublishDate = new DateTime(2001,06,12)
-        },
-        new Book{
-            Id = 2, 
-            Title = "Herland", 
-            GenreId = 2, //Science Fiction
-            PageCount = 250,
-            PublishDate = new DateTime(2010,05,23)
-        },
-        new Book{
-            Id = 3, 
-            Title = "Dune", 
-            GenreId = 2, //Science Fiction
-            PageCount = 540,
-            PublishDate = new DateTime(2001,12,21)
-        }
-    };
+        _context = context;
+    }
+
+
+
+    // private static List<Book> BookList = new List<Book>()
+    // {
+    //     new Book{
+    //         Id = 1, 
+    //         Title = "Lean Startup", 
+    //         GenreId = 1, //Personal Growth
+    //         PageCount = 200,
+    //         PublishDate = new DateTime(2001,06,12)
+    //     },
+    //     new Book{
+    //         Id = 2, 
+    //         Title = "Herland", 
+    //         GenreId = 2, //Science Fiction
+    //         PageCount = 250,
+    //         PublishDate = new DateTime(2010,05,23)
+    //     },
+    //     new Book{
+    //         Id = 3, 
+    //         Title = "Dune", 
+    //         GenreId = 2, //Science Fiction
+    //         PageCount = 540,
+    //         PublishDate = new DateTime(2001,12,21)
+    //     }
+    // };
     [HttpGet] // [HttpGet] parametresiz sadece bir defa çalışabilir.
     public List<Book> GetBooks(){
-        return BookList;
+        return _context.Books.ToList();
     }
 
     [HttpGet("{id}")]
-    public Book GetById(int id){
-        return BookList.Where(book => book.Id == id).SingleOrDefault();
+    public Book? GetById(int id){
+        return _context.Books.Where(book => book.Id == id).SingleOrDefault();
     }
 
     [HttpPost]
     public IActionResult AddBook([FromBody] Book newBook){
-        var book = BookList.
+        var book = _context.Books.
             SingleOrDefault(x => x.Title == newBook.Title);
         
         if(book is not null)
             return BadRequest();
         
-        BookList.Add(newBook);
+        _context.Books.Add(newBook);
+        _context.SaveChanges();
         return Ok();
 
     }
 
     [HttpPut("{id}")]
     public IActionResult UpdateBook(int id, [FromBody] Book updatedBook){
-        var book = BookList.SingleOrDefault(x => x.Id == id);
+        var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
         if(book is null)
             return BadRequest();
@@ -65,19 +76,20 @@ public class BookController : ControllerBase
         book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
         book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
         book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
-
+        _context.SaveChanges();
         return Ok();
 
     }
 
     [HttpDelete("{id}")]
     public IActionResult DeleteBook(int id){
-        var book = BookList.SingleOrDefault(x => x.Id == id);
+        var book = _context.Books.SingleOrDefault(x => x.Id == id);
 
         if(book is null)
             return BadRequest();
         
-        BookList.Remove(book);
+        _context.Books.Remove(book);
+        _context.SaveChanges();
         return Ok();
     }
 
